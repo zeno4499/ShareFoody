6766c4605b7e519c82350e4af2ae4caf66b33fe7
snpham1102 <snpham1102@gmail.com>
2020-11-01 22:08:34 +0700

    no message

--- FoodyRealTime/app/src/main/java/com/example/foodyrealtime/View/HomeActivity.java
+++ FoodyRealTime/app/src/main/java/com/example/foodyrealtime/View/HomeActivity.java
@@ -5,15 +5,17 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.foodyrealtime.Adapter.AdapterViewPagerHomeApp;
 import com.example.foodyrealtime.R;
 
-public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, RadioGroup.OnCheckedChangeListener {
 
     ViewPager viewPagerHomeApp;
     RadioButton rd_odau;
     RadioButton rd_angi;
+    RadioGroup radioGroup_oDau_anGi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,11 @@ public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageC
         viewPagerHomeApp = findViewById(R.id.viewPagerHomeApp);
         rd_angi = findViewById(R.id.angi);
         rd_odau = findViewById(R.id.odau);
+        radioGroup_oDau_anGi = findViewById(R.id.radioGroup_anGi_oDau);
+
         AdapterViewPagerHomeApp adapterViewPagerHomeApp = new AdapterViewPagerHomeApp(getSupportFragmentManager());
         viewPagerHomeApp.setAdapter(adapterViewPagerHomeApp);
+        radioGroup_oDau_anGi.setOnCheckedChangeListener(this);
         viewPagerHomeApp.addOnPageChangeListener(this);
     }
 
@@ -49,5 +54,19 @@ public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageC
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.angi: {
+                viewPagerHomeApp.setCurrentItem(1);
+                break;
+            }
+            case R.id.odau: {
+                viewPagerHomeApp.setCurrentItem(0);
+                break;
+            }
+        }
     }
 }
