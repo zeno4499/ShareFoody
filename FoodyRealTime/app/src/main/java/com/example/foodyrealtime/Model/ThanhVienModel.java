6f8e9686a8123e71f74c161b440186cd9fdbd16b
snpham1102 <snpham1102@gmail.com>
2020-11-04 19:03:07 +0700

    no message

--- FoodyRealTime/app/src/main/java/com/example/foodyrealtime/Model/ThanhVienModel.java
+++ FoodyRealTime/app/src/main/java/com/example/foodyrealtime/Model/ThanhVienModel.java
@@ -3,20 +3,12 @@ package com.example.foodyrealtime.Model;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-<<<<<<< Updated upstream
-public class ThanhVienModel {
-    private DatabaseReference dataNodeThanhvien;
-    private String hoten, hinhanh;
-
-    public ThanhVienModel() {
-=======
 public class ThanhvienModel {
     private DatabaseReference dataNodeThanhvien;
     private String hoten,
             hinhanh;
 
     public ThanhvienModel() {
->>>>>>> Stashed changes
         dataNodeThanhvien = FirebaseDatabase.getInstance().getReference().child("thanhviens");
     }
 
@@ -39,10 +31,5 @@ public class ThanhvienModel {
 
         dataNodeThanhvien.child(uid).setValue(thanhvienModel);
 
-<<<<<<< Updated upstream
-    public void themthongtinthanhvien(ThanhVienModel thanhvienModel, String uid) {
-        dataNodeThanhvien.child(uid).setValue(thanhvienModel);
-=======
->>>>>>> Stashed changes
     }
 }
